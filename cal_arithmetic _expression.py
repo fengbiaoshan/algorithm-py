f4b26e012798d334ebf77a2e7f6280029f3e647e
Mi <fengbiaoshan@users.noreply.github.com>
2017-09-24 20:37:43 +0800

    consider negative and minus

--- cal_arithmetic _expression.py
+++ cal_arithmetic _expression.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
 priority = [  
-    ['<','<','<','<','>',''],   # +  
-    ['<','<','<','<','>',''],   # -  
-    ['>','>','<','<','>',''],   # *  
-    ['>','>','<','<','>',''],   # /  
-    ['>','>','>','>','>',''],   # (  
-    ['<','<','<','<','=',''],   # )   
-]    #+   -   *   /   (   )
+    ['<','<','<','<','>','','<'],   # +  
+    ['<','<','<','<','>','','<'],   # -  
+    ['>','>','<','<','>','','<'],   # *  
+    ['>','>','<','<','>','','<'],   # /  
+    ['>','>','>','>','>','','>'],   # (  
+    ['<','<','<','<','=','', ''],   # ) 
+    ['>','>','>','>','>','', '']    # --
+]    #+   -   *   /   (   )  --
 
+# '--' is negative   '-' is minus
 
-opindex = {'+':0, '-':1, '*':2, '/':3, '(':4, ')':5}
+opindex = {'+':0, '-':1, '*':2, '/':3, '(':4, ')':5, "--":6}
 
 operandstack = []
 operatorstack = []
@@ -30,6 +32,7 @@ def compare_priority(notation, stacktop):
 
 def cal(expression):
     i = 0
+    lastc = 1 #for recognize negative and minus
     while i < len(expression):
         operand = ""
         while (i < len(expression)) and (expression[i] not in opindex):
@@ -37,28 +40,42 @@ def cal(expression):
             i += 1
         if operand != "":
             operandstack.append(float(operand))
+            lastc = 0
         while i < len(expression):
-            if not operatorstack:
-                operatorstack.append(expression[i])
-                break
-            elif compare_priority(expression[i], operatorstack[-1]) == ">":
-                operatorstack.append(expression[i])
+            if not operatorstack \
+               or (compare_priority(expression[i], operatorstack[-1]) == ">"):
+                if expression[i] == "-" and lastc == 1:
+                    operatorstack.append("--")
+                else:
+                    operatorstack.append(expression[i])
+                if expression[i] == "(":
+                    lastc = 1
                 break
             elif compare_priority(expression[i], operatorstack[-1]) == "<":
-                result = operation(operandstack.pop(),
-                                   operandstack.pop(),operatorstack.pop())
-                operandstack.append(result)
+                opr1 = operandstack.pop()
+                op = operatorstack.pop()
+                if op == "--":
+                    operandstack.append(-opr1)
+                else:
+                    opr2 = operandstack.pop()
+                    result = operation(opr1, opr2, op)
+                    operandstack.append(result)
             elif compare_priority(expression[i], operatorstack[-1]) == "=":
                 operatorstack.pop()
                 break
         i += 1
     while operatorstack:
-        result = operation(operandstack.pop(),
-                           operandstack.pop(),operatorstack.pop())
-        operandstack.append(result)
+        opr1 = operandstack.pop()
+        op = operatorstack.pop()
+        if op == "--":
+            operandstack.append(-opr1)
+        else:
+            opr2 = operandstack.pop()
+            result = operation(opr1, opr2, op)
+            operandstack.append(result)
     return operandstack.pop()
 if __name__ == "__main__":
-    expre = "5*(8+7)*10"
+    expre = "(1-(4+5+2)-3)+(-(6-3)+8)"
     print cal(expre)
     
 
